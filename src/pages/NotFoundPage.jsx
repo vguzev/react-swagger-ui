688840df4e2af1257e90d486c4c6cd47aa4e896d
Vadim Guzev <vguzev@yandex.ru>
2016-12-17 17:21:57 +0300

    Switched to react-boilerplate

--- src/pages/NotFoundPage.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class NotFoundPage extends Component {
-    render () {
-        return (
-            <div>Oups, page not found!</div>
-        )
-    }
-}
